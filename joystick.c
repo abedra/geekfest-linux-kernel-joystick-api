75e76c0a25544ad12497b595e43e46825f0efc57
Aaron Bedra <aaron@aaronbedra.com>
2012-08-20 22:01:26 -0500

    full example

--- joystick.c
+++ joystick.c
@@ -231,13 +231,13 @@ process the events and keep reading it until you empty the driver queue.
 
 The joystick driver defines the following ioctl(2) operations.
 
-				// function			3rd arg  
-	#define JSIOCGAXES	// get number of axes		char	 
-	#define JSIOCGBUTTONS	// get number of buttons	char	 
-	#define JSIOCGVERSION	// get driver version		int	 
-	#define JSIOCGNAME(len) // get identifier string	char	 
-	#define JSIOCSCORR	// set correction values	&js_corr 
-	#define JSIOCGCORR	// get correction values	&js_corr 
+				// function			3rd arg
+	#define JSIOCGAXES	// get number of axes		char
+	#define JSIOCGBUTTONS	// get number of buttons	char
+	#define JSIOCGVERSION	// get driver version		int
+	#define JSIOCGNAME(len) // get identifier string	char
+	#define JSIOCSCORR	// set correction values	&js_corr
+	#define JSIOCGCORR	// get correction values	&js_corr
 
 For example, to read the number of axes
 
